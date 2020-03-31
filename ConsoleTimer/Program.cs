ab372c73a137739683d83d168066bfb6b59fc18e
cainy-a <cainatkinson98@gmail.com>
2020-03-31 09:14:12 +0100

    Minutes and Seconds work for Stopwatch

---
+++ ConsoleTimer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ConsoleTimer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+        restart:;
+            Console.Write("Would you like to countdown [down] or count up [up]?");
+            var upOrDown = Console.ReadLine();
+
+            switch (upOrDown.ToLower())
+            {
+                case "up":
+                    countUp();
+                    break;
+                case "down":
+                    Console.Write("How many hours would you like to count down for?");
+                    int hours = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("How many minutes would you like to count down for?");
+                    int minutes = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("How many seconds would you like to count down for?");
+                    float seconds = (float)Convert.ToDouble(Console.ReadLine());
+
+                    countDown(hours, minutes, seconds);
+                    break;
+                default:
+                    Console.Clear();
+                    goto restart;
+            }
+        }
+
+        private static void countUp()
+        {
+            float secondsRaw = 0;
+            float secondsProcessed = 0;
+            int minutes = 0;
+            int hours = 0;
+
+            while (true)
+            {
+                ProcessTime(secondsRaw,
+                    out hours,
+                    out minutes,
+                    out secondsProcessed);
+                Console.Clear();
+                Console.WriteLine($"{hours}:{minutes}:{secondsProcessed}");
+                secondsRaw += 0.1f;
+                secondsRaw = (float)Math.Round(secondsRaw, 1);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private static void ProcessTime(float secondsIn, out int hoursOut, out int minutesOut, out float secondsOut)
+        {
+            if (secondsIn < 60)
+            {
+                hoursOut = 0;
+                minutesOut = 0;
+                secondsOut = secondsIn;
+            }
+            else if (secondsIn >= 60 && secondsIn < 360)
+            {
+                hoursOut = 0;
+                minutesOut = (int)Math.Floor(secondsIn / 60);
+                secondsOut = (float)Math.Round(secondsIn - (Math.Floor(secondsIn / 60) * 60), 1);
+            }
+            else
+            {
+                hoursOut = 0;
+                minutesOut = 0;
+                secondsOut = 0;
+            }
+        }
+
+        private static void countDown(long targetHours, int targetMinutes, float targetSeconds)
+        {
+
+        }
+    }
+}
